830db9243bccb855c64f757a01921beba53e46a4
Your Name <you@example.com>
2026-07-20 14:24:52 +0300

    iter6

--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module url-shortener-practicum-go
 go 1.26.4
 
 require github.com/go-chi/chi v1.5.5
+
+require (
+	go.uber.org/multierr v1.10.0
+	go.uber.org/zap v1.28.0
+)
--- internal/server/server.go
+++ internal/server/server.go
@@ -2,11 +2,14 @@ package server
 
 import (
 	"github.com/go-chi/chi"
+	"go.uber.org/zap"
 	"url-shortener-practicum-go/internal/handlers"
+	"url-shortener-practicum-go/internal/middleware"
 )
 
-func NewRouter(h *handlers.Handler) *chi.Mux {
+func NewRouter(h *handlers.Handler, log *zap.Logger) *chi.Mux {
 	r := chi.NewRouter()
+	r.Use(middleware.Logger(log))
 	r.Post("/", h.ShortenURL)
 	r.Get("/{id}", h.Redirect)
 	return r
